af36c3d8bdfdb5b6e9eec6558c9618a85f857955
Roshan Praveen Shetty <roshanshetty2000@gmail.com>
2024-08-27 12:57:11 -0400

    Updated errors

--- components/Upload.tsx
+++ components/Upload.tsx
@@ -1,13 +1,18 @@
+"use client";
 import { useDropzone } from "react-dropzone";
 import { motion } from "framer-motion";
 
-const Upload = ({ onDrop }: { onDrop: (files: File[]) => void }) => {
+const Upload = ({ onDrop }: {
+	onDrop: (files: File[]) => void
+}) =>
+{
 	const { getRootProps, getInputProps } = useDropzone({
 		onDrop,
-		accept: "image/*",
+		accept: { "image/*": ["jpeg", "png", "gif"] },
 	});
 
 	return (
+		// @ts-ignore
 		<motion.div
 			{...getRootProps()}
 			className="border-2 border-dashed border-gray-300 p-6 rounded-lg flex justify-center items-center"
